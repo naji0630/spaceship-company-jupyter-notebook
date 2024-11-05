796cacf52a3a3c9aa4f28c56927893215841aa01
jihoon.na <jihoon.na@woowahan.com>
2024-11-06 08:20:18 +0900

    ㅁ

--- inference/main.py
+++ inference/main.py
@@ -7,10 +7,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
-# FastAPI 앱 생성
 app = FastAPI()
 
-# 모델 및 데이터 불러오기
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
@@ -22,7 +20,6 @@ with open('name_probability_dict.pickle', 'rb') as handle:
 
 model = load_model('gender_prediction_model.h5')
 
-# 성별 확률 예측 함수
 def predict_gender_probability(name: str) -> float:
     if not name:
         return 0.5  
@@ -36,18 +33,15 @@ def predict_gender_probability(name: str) -> float:
         probability = model.predict(padded_sequence)[0][0]
     return float(probability)
 
-# 요청 모델
 class NameRequest(BaseModel):
     name: str
 
-# 성별 확률 예측 엔드포인트
 @app.post("/predict-gender")
 async def predict_gender(request: NameRequest):
     name = request.name
     probability = predict_gender_probability(name)
     return {"name": name, "gender_probability": probability}
 
-# FastAPI 서버 실행
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
